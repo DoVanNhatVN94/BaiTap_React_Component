5220f4637d6a68dc982eae8984d9dddf817b986c
DoVanNhatVN94 <dovannhatdn94@gmail.com>
2022-02-22 21:33:58 +0700

    up2

--- project/src/BaiTapLayoutComponent/HeaderComponent.js
+++ project/src/BaiTapLayoutComponent/HeaderComponent.js
@@ -8,7 +8,7 @@ export default class HeaderComponent extends Component {
     <div className="p-4 p-lg-5 bg-light rounded-3 ">
       <div className="m-4 m-lg-5">
         <h1 className="display-5">A warm welcome!</h1>
-        <p className="fs-4">
+        <p className="fs-4 text text-black-50">
          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam corrupti vero beatae, a dolorem fuga, ullam nemo magni recusandae, laudantium consequatur!
         </p>
         <a className="btn btn-primary btn-lg" href="#!">
