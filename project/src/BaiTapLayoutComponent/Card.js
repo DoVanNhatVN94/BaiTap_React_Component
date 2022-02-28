cf78ebd34ad167bba6e93fb6252a47367c19886d
DoVanNhatVN94 <dovannhatdn94@gmail.com>
2022-02-28 21:57:44 +0700

    fix css

--- project/src/BaiTapLayoutComponent/Card.js
+++ project/src/BaiTapLayoutComponent/Card.js
@@ -3,16 +3,16 @@ import React, { Component } from "react";
 export default class Card extends Component {
   render() {
     return (
-      <div className="p-5 col-3">
-        <div className="card" style={{ width: "18rem" }}>
+      <div className="col-3 ">
+        <div className="card">
           <div className="card-head">
-              <span>500 x 325</span>
+            <span>500 x 325</span>
           </div>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-              provident ipsa maxime tempore amet dolorem.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Delectus, maxime!
             </p>
             <a href="#" className="btn btn-primary">
               Find Out More
--- project/src/BaiTapLayoutComponent/ContentComponent.js
+++ project/src/BaiTapLayoutComponent/ContentComponent.js
@@ -4,12 +4,12 @@ import Card from "./Card";
 export default class ContentComponent extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container px-lg-5 pb-5">
         <div className="row justify-content-center">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
         </div>
       </div>
     );
